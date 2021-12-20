fa45729ad0306211d9bdb1b3b7eafc44626b8de6
yuyun-zhao <im.yuyunzhao@gmail.com>
2021-12-20 21:01:46 +0800

    feat：初始化工程

---
+++ mall-coupon/src/test/java/com/zhao/yunmall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhao.yunmall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
