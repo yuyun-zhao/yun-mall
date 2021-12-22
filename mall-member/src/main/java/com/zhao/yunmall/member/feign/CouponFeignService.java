dfac837ff39ba1da74f857ecc01448f5f6efd3bd
yuyun-zhao <im.yuyunzhao@gmail.com>
2021-12-22 14:51:14 +0800

    fit：增加优惠券模块、会员模块、订单模块、商品模块以及仓储模块的基础业务；创建公共模块管理其他子模块的依赖

---
+++ mall-member/src/main/java/com/zhao/yunmall/member/feign/CouponFeignService.java
@@ -0,0 +1,8 @@
+package com.zhao.yunmall.member.feign;
+
+/**
+ * @author yuyun zhao
+ * @date 2021/12/22 14:46
+ */
+public class CouponFeignService {
+}
--- mall-product/src/test/java/com/zhao/yunmall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/zhao/yunmall/product/MallProductApplicationTests.java
@@ -1,13 +1,30 @@
 package com.zhao.yunmall.product;
 
-import org.junit.jupiter.api.Test;
+import com.zhao.yunmall.product.entity.BrandEntity;
+import com.zhao.yunmall.product.service.BrandService;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@Data
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class MallProductApplicationTests {
+public class MallProductApplicationTests {
+
+    @Autowired
+    BrandService brandService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setDescript("快手");
+
+        brandService.save(brandEntity);
     }
 
 }
